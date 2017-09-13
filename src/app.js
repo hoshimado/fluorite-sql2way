af58731bdd2a5902fc203b66b8815bdfdcd00b1e
xingyanhuan <xingyanhuan@yahoo.co.jp>
2017-09-13 22:15:50 +0900

    Expressフレームワーク
    静的htmlファイルを、Expressフレームワークに置いてみた。

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var api_v1_activitylog_show = require('./roues/show'); // ★追加 for RESTful API★
+
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/v1/activitylog/show', api_v1_activitylog_show ); // ★追加 for RESTful API★
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

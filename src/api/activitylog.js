9879edecd19d878d5f4eb99cea92a337a9f1f985
xingyanhuan <xingyanhuan@yahoo.co.jp>
2017-09-19 07:56:59 +0900

    SQLアクセスの部分を共通化（まだ途中）

--- src/api/activitylog.js
+++ src/api/activitylog.js
@@ -45,6 +45,107 @@ exports.API_PARAM = API_PARAM;
 
 
 
+
+var API_V1_BASE = function(){
+	this._outJsonData = {};
+};
+API_V1_BASE.prototype.isOwnerValid = function( inputData ){
+	// 接続元の認証Y/Nを検証。
+	var outJsonData = this._outJsonData;
+	var paramClass = new API_PARAM( inputData );
+
+	return new Promise(function(resolve,reject){
+		var config = factoryImpl.CONFIG_SQL.getInstance();
+		var isOwnerValid = factoryImpl.sql_parts.getInstance( "isOwnerValid" );
+		var is_onwer_valid_promise = isOwnerValid( 
+			config.database, 
+			paramClass.getDeviceKey() 
+		);
+		is_onwer_valid_promise.then(function( maxCount ){
+			resolve( paramClass ); // ⇒次のthen()が呼ばれる。
+		}).catch(function(err){
+			if( err ){
+				outJsonData[ "errer_on_validation" ] = err;
+			}
+			reject({
+				"http_status" : 401 // Unauthorized
+			}); // ⇒次のcatch()が呼ばれる。
+		});
+	});	
+};
+API_V1_BASE.prototype.isAccessRateValied = function(){};
+API_V1_BASE.prototype.requestSql = function( paramClass ){
+	var outJsonData = this._outJsonData;
+	// paramClass =>
+	// API_PARAM.prototype.getDeviceKey = function(){ return isDefined( this, "device_key"); };
+	// API_PARAM.prototype.getTypeValue = function(){ return isDefined( this, "type_value"); };
+	// API_PARAM.prototype.getStartDate = function(){ return isDefined( this, "date_start"); };
+	// API_PARAM.prototype.getEndDate   = function(){ return isDefined( this, "date_end"); };
+	// API_PARAM.prototype.getMaxCount = function(){ return isDefined( this, "max_count"); };
+};
+API_V1_BASE.prototype.closeNormal = function(){};
+API_V1_BASE.prototype.closeAnomaly = function(){};
+API_V1_BASE.prototype.run = function( inputData ){ // getほげほげObjectFromGetData( queryFromGet );済み。
+	var outJsonData = this._outJsonData;
+	var createPromiseForSqlConnection = factoryImpl.sql_parts.getInstance().createPromiseForSqlConnection;
+
+	if( inputData.invalid && inputData.invalid.length > 0 ){
+		outJsonData[ "error_on_format" ] = "GET or POST format is INVAILD.";
+		return Promise.resolve({
+			"jsonData" : outJsonData,
+			"status" : 400 // Bad Request
+		});
+	}
+
+  return createPromiseForSqlConnection( 
+		outJsonData, 
+		inputData, 
+		factoryImpl.CONFIG_SQL.getInstance()
+	).then(function( inputData ){
+		return this.isOwnerValid( inputData );
+  }).then(function( paramClass ){
+		// this.isAccessRateValied()
+		return Promise.resolve( paramClass );
+  }).then(function( paramClass ){
+		return this.requestSql( paramClass );
+	});
+};
+var activitylog_show = function( queryFromGet, dataFromPost ){
+	var API_SHOW = function(){
+		var self = this;
+		// API_V1_BASE.call( this, constaractParam ); // 継承元のコンスタラクタを明示的に呼び出す。
+	};
+	API_SHOW.prototype = Object.create( API_V1_BASE.prototype );
+	API_SHOW.prototype.requestSql = function( paramClass ){
+		// 対象のログデータをSQLへ要求
+		var param = new API_PARAM( inputData );
+		var config = factoryImpl.CONFIG_SQL.getInstance();
+    var getListOfActivityLogWhereDeviceKey = factoryImpl.sql_parts.getInstance().getListOfActivityLogWhereDeviceKey;
+
+		return new Promise(function(resolve,reject){
+			getListOfActivityLogWhereDeviceKey(
+				config.database, 
+				param.getDeviceKey(), 
+				{ 
+					"start" : param.getStartDate(), 
+					"end"   : param.getEndDate()
+				}
+			).then(function(recordset){
+        // ログ取得処理が成功
+				// 【FixME】総登録数（対象のデバイスについて）を取得してjsonに含めて返す。取れなければ null でOK（その場合も成功扱い）。
+				outJsonData["table"] = recordset;
+				resolve();
+			}).catch(function(err){
+				// 取得処理で失敗。
+				outJsonData[ "error_on_insert" ];
+				reject( err ); // ⇒次のcatch()が呼ばれる。
+			});
+		});
+	};
+};
+
+
+
 /**
  * バッテリーログをSQLから、指定されたデバイス（のハッシュ値）のものを取得する。
  */
@@ -174,7 +275,6 @@ exports.api_v1_activitylog_show = function( queryFromGet, dataFromPost ){
     });
   });
 };
-
 
 
 

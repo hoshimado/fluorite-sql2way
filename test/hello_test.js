1cbb4ea0c3c5d864f94c26b141f7dbd31bc54002
xingyanhuan <xingyanhuan@yahoo.co.jp>
2017-10-08 16:57:53 +0900

    テストコードから不要な部分を削除

--- test/hello_test.js
+++ test/hello_test.js
@@ -7,27 +7,11 @@
 var chai = require("chai");
 var assert = chai.assert;
 var expect = chai.expect;
-var sinon = require("sinon");
 var shouldFulfilled = require("promise-test-helper").shouldFulfilled;
 var shouldRejected  = require("promise-test-helper").shouldRejected;
 
 const hello = require("../src/api/hello.js");
 
-
-exports.world = function( queryFromGet, dataFromPost ){
-    var name = queryFromGet.name;
-    var out_data = { 
-        "status" : 200,
-        "jsonData" : "hello world, " + name + "!" 
-    };
-    var promise = new Promise(function(resolve,reject){
-        setTimeout(function() {
-            // 非同期の処理を模している。
-            resolve( out_data );
-        }, 100);
-    });
-    return promise;
-};
 
 describe( "hello.js", function(){
     var helloWorld = hello.world;

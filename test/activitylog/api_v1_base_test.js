84e16d9f1d8c3208db3062be9af1d8b93a95225b
xingyanhuan <xingyanhuan@yahoo.co.jp>
2018-01-19 12:51:29 +0900

    api_v1_baseのテストを分かり易くした
    Strategyとしてコンスタラクタに、利用する外部I/Fを渡す＝中でフックする必要が無い、事を明確にした。

--- test/activitylog/api_v1_base_test.js
+++ test/activitylog/api_v1_base_test.js
@@ -14,34 +14,31 @@ var shouldRejected  = require("promise-test-helper").shouldRejected;
 require('date-utils');
 var ApiCommon_StubAndHooker = require("../support_stubhooker.js").ApiCommon_StubAndHooker;
 
-const api_v1_base = require("../../src/api/activitylog/api_v1_base.js");
+var api_v1_base = require("../../src/api/activitylog/api_v1_base.js");
+
 
 var TEST_CONFIG_SQL = { // テスト用
 	database : "fake_db_name.sqlite3"
 };
+var lib = require("../../src/api/factory4require.js");
+var strategy_hook = {
+    "factoryImpl" : { // require()を使う代わりに、new Factory() する。
+        "sql_parts" : new lib.Factory4Require("./sql_db_io/index.js"),
+        "CONFIG_SQL" : new lib.Factory({"database" : TEST_CONFIG_SQL})
+    }
+};
+
 
 
 describe( "api_v1_base.js", function(){
-
     var COMMON_STUB_MANAGER = new ApiCommon_StubAndHooker(function(){
         return {
             "CONFIG_SQL" : TEST_CONFIG_SQL, 
-            "sql_parts" : {
+            "sql_parts" : { // API_V1_BASEへ渡すStrategyは、以下のメソッドを持てばよい。
                 "createPromiseForSqlConnection" : sinon.stub(),
                 "closeConnection" : sinon.stub(),
-                "isOwnerValid" : sinon.stub(),
-                "getNumberOfUsers" : sinon.stub(),
-                "setupTable1st" : sinon.stub(),
-                "addNewUser" : sinon.stub(),
-                "deleteExistUser" : sinon.stub(),
-                "getInsertObjectFromPostData" : sinon.stub(),
-                "getShowObjectFromGetData" : sinon.stub(),
-                "getDeleteObjectFromPostData" : sinon.stub(), 
-                "getNumberOfLogs" : sinon.stub(),
-                "addActivityLog2Database" : sinon.stub(),
-                "getListOfActivityLogWhereDeviceKey" : sinon.stub(),
-                "deleteActivityLogWhereDeviceKey" : sinon.stub()
-            }
+                "isOwnerValid" : sinon.stub()
+            } // 逆に、この3つの実装がデグレした場合は、このテストドライバーのhookInstance()のところでNGになるハズ。
         };
     });
 
@@ -49,30 +46,24 @@ describe( "api_v1_base.js", function(){
         var stubs;
         var API_V1_BASE = api_v1_base.API_V1_BASE;
 
-        // ◆ToDo◆：わざわざhookなんぞせずにもっと分り安いテストドライバー方法があるはず。
-        var lib = require("../../src/api/factory4require.js");
-        var hook = {
-            "factoryImpl" : { // require()を使う代わりに、new Factory() する。
-                "sql_parts" : new lib.Factory4Require("./sql_db_io/index.js"),
-                "CONFIG_SQL" : new lib.Factory({"database" : TEST_CONFIG_SQL})
-            }
-        };
-
         /**
          * @type beforeEachで初期化される。
          */
         beforeEach(function(){ // 内部関数をフックする。
             stubs = COMMON_STUB_MANAGER.createStubs();
 
-            COMMON_STUB_MANAGER.hookInstance( hook, stubs );
+            COMMON_STUB_MANAGER.hookInstance( strategy_hook, stubs );
         });
         afterEach(function(){
-            COMMON_STUB_MANAGER.restoreOriginal( hook );
+            COMMON_STUB_MANAGER.restoreOriginal( strategy_hook );
         });
 
         // ここからテスト。
         it("正常系", function(){
-            var instance = new API_V1_BASE( hook.factoryImpl.CONFIG_SQL, hook.factoryImpl.sql_parts );
+            var instance = new API_V1_BASE( 
+                strategy_hook.factoryImpl.CONFIG_SQL, 
+                strategy_hook.factoryImpl.sql_parts 
+            );
             var spied_requestSql = sinon.spy( instance, "requestSql" );
             var inputData = {
                 "device_key" : "これは識別キー。必ず必要",

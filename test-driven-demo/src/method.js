eb2944d092c5160d3a96112eec9447eafec724b2
xingyanhuan <xingyanhuan@yahoo.co.jp>
2018-07-05 07:14:15 +0900

    ゼロから～でsqlite3利用の例を考えてみる
    が、sqliteのnewのところで手が止まった。。。

--- test-driven-demo/src/method.js
+++ test-driven-demo/src/method.js
@@ -2,6 +2,9 @@
  * [method.js]
  */
 
+var createHookPoint = require("../../hook-test-helper").createHookPoint;
+var sqlite3 = createHookPoint(exports,"sqlite3",require("sqlite3"));
+
 
 exports.getAssociatedKey = function ( baseKey ) {
     return Promise.resolve();
--- test-driven-demo/test/method_test.js
+++ test-driven-demo/test/method_test.js
@@ -9,15 +9,39 @@ var expect = chai.expect;
 var sinon = require("sinon");
 var shouldFulfilled = require("promise-test-helper").shouldFulfilled;
 var shouldRejected  = require("promise-test-helper").shouldRejected;
-// var hookProperty = require("../../hook-test-helper").hookProperty;
+var hookProperty = require("../../hook-test-helper").hookProperty;
 
 var target = require("../src/method.js");
 
 describe("method.js", function(){
     describe("getAssociatedKey()",function () {
         var authenticateKey = target.getAssociatedKey;
+        var hooked = {}, stub_sqlite3;
+        var STUB_DATABASE = function (databaseName, callback) {
+            this._dateBaseName = databaseName;
+            setTimeout(() => {
+                callback(); // 成功版なので、引数無し。
+            }, 100);
+        };
+        STUB_DATABASE.prototype.all = function (queryStr, paramsArray, callback) {
+            // callback(err, rows)
+        };
+        STUB_DATABASE.prototype.close = function (callback) {
+            // callback(err);
+        };
+
+        beforeEach(function () {
+            stub_sqlite3 = {
+                "Database" : STUB_DATABASE
+            };
+            hooked["sqlite3"] = hookProperty(target.sqlite3,stub_sqlite3);
+        });
+        afterEach(function () {
+            hooked["sqlite3"].restore();
+        })
         it("success to get the associated key with the base key.",function () {
             var INPUT_KEY = "元に成るキー";
+
             return shouldFulfilled(
                 authenticateKey( INPUT_KEY )
             ).then(function (result) {

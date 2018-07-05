ab6b4afd2b4e855c54f09184d5abc849f70e6389
hoshimado <xingyanhuan@yahoo.co.jp>
2018-07-05 18:32:38 +0900

    newをstub越しに検証する、と試みる。動作は未確認。

--- test-driven-demo/test/method_test.js
+++ test-driven-demo/test/method_test.js
@@ -11,40 +11,54 @@ var shouldFulfilled = require("promise-test-helper").shouldFulfilled;
 var shouldRejected  = require("promise-test-helper").shouldRejected;
 var hookProperty = require("../../hook-test-helper").hookProperty;
 
-var target = require("../src/method.js");
 
 describe("method.js", function(){
+    var target = require("../src/method.js");
+
     describe("getAssociatedKey()",function () {
         var authenticateKey = target.getAssociatedKey;
-        var hooked = {}, stub_sqlite3;
-        var STUB_DATABASE = function (databaseName, callback) {
-            this._dateBaseName = databaseName;
+        var hooked = {};
+        var stub_sqlite3_instance;
+        var DUMMY_SQLITE3_VERBOSE = function(){};
+        DUMMY_SQLITE3_VERBOSE.prototype.Database = function (DatabaseName,callback) {
             setTimeout(() => {
-                callback(); // 成功版なので、引数無し。
+                callback(
+                    stub_sqlite3_instance._constructor( DatabaseName )
+                );
             }, 100);
+            return stub_sqlite3_instance; // newで呼ばれた場合のデフォルト this に代えて、これを返却する。
         };
-        STUB_DATABASE.prototype.all = function (queryStr, paramsArray, callback) {
-            // callback(err, rows)
-        };
-        STUB_DATABASE.prototype.close = function (callback) {
-            // callback(err);
-        };
+        var stub_sqlite3_verbose = new DUMMY_SQLITE3_VERBOSE();
+        var stub_sqlite3_databese = sinon.stub(stub_sqlite3_verbose,"Database");
 
         beforeEach(function () {
-            stub_sqlite3 = {
-                "Database" : STUB_DATABASE
+            stub_sqlite3_instance = {
+                "_constructor" : sinon.stub(),
+                "all" : sinon.stub(),
+                "close" : sinon.stub()
             };
-            hooked["sqlite3"] = hookProperty(target.sqlite3,stub_sqlite3);
+            hooked["sqlite3"] = hookProperty(target.sqlite3,{
+                "verbose" : function(){
+                    return {
+                        "Database" : stub_sqlite3_databese
+                    };
+                }
+            });
+            stub_sqlite3_databese.reset();
         });
         afterEach(function () {
             hooked["sqlite3"].restore();
-        })
+        });
         it("success to get the associated key with the base key.",function () {
             var INPUT_KEY = "元に成るキー";
 
             return shouldFulfilled(
                 authenticateKey( INPUT_KEY )
             ).then(function (result) {
+                // assert(stub_sqlite3_databese.Database.calledWithNew(), "sqlite3.verbose.Database()をnewしてインスタンスを生成する。");
+                // spy.calledWithNew();
+                // Returns true if spy/stub was called the new operator.
+
                 expect(result).to.have.property("associated_key");
             });
         });
